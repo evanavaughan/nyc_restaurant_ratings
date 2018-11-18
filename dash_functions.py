c2cd25d5e0342e1a5437aee7b908f6351f52e4b4
kahartman2 <41496627+kahartman2@users.noreply.github.com>
2018-11-17 19:14:42 -0500

    Add files via upload

---
+++ dash_functions.py
@@ -0,0 +1,89 @@
+from sqlalchemy import create_engine, func
+from __init__ import db
+from db_models import Restaurant, TripAdvisor, Infatuation, Neighborhood
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import func
+
+engine = create_engine('sqlite:///nyc_restaurants.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def inf_price_rating_hist_1():
+    less_two = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating < 2).filter(Infatuation.price == '$').all()[0][0]
+    two_to_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 2).filter(Infatuation.rating < 4).filter(Infatuation.price == '$').all()[0][0]
+    greater_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 4).filter(Infatuation.price == '$').all()[0][0]
+    return [less_two, two_to_four, greater_four]
+
+def inf_price_rating_hist_2():
+    less_two = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating < 2).filter(Infatuation.price == '$$').all()[0][0]
+    two_to_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 2).filter(Infatuation.rating < 4).filter(Infatuation.price == '$$').all()[0][0]
+    greater_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 4).filter(Infatuation.price == '$$').all()[0][0]
+    return [less_two, two_to_four, greater_four]
+
+def inf_price_rating_hist_3():
+    less_two = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating < 2).filter(Infatuation.price == '$$$').all()[0][0]
+    two_to_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 2).filter(Infatuation.rating < 4).filter(Infatuation.price == '$$$').all()[0][0]
+    greater_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 4).filter(Infatuation.price == '$$$').all()[0][0]
+    return [less_two, two_to_four, greater_four]
+
+def inf_price_rating_hist_4():
+    less_two = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating < 2).filter(Infatuation.price == '$$$$').all()[0][0]
+    two_to_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 2).filter(Infatuation.rating < 4).filter(Infatuation.price == '$$$$').all()[0][0]
+    greater_four = db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.rating >= 4).filter(Infatuation.price == '$$$$').all()[0][0]
+    return [less_two, two_to_four, greater_four]
+
+def ta_price_rating_hist_1():
+    less_two = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating < 4.25).filter(TripAdvisor.price == '$').all()[0][0]
+    two_to_four = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.25).filter(TripAdvisor.rating < 4.75).filter(TripAdvisor.price == '$').all()[0][0]
+    greater_four = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.75).filter(TripAdvisor.price == '$').all()[0][0]
+    return [less_two, two_to_four, greater_four]
+
+def ta_price_rating_hist_2():
+    less_two = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating < 4.25).filter(TripAdvisor.price == '$$ - $$$').all()[0][0]
+    two_to_four = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.25).filter(TripAdvisor.rating < 4.75).filter(TripAdvisor.price == '$$ - $$$').all()[0][0]
+    greater_four = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.75).filter(TripAdvisor.price == '$$ - $$$').all()[0][0]
+    return [less_two, two_to_four, greater_four]
+
+# def ta_price_rating_hist_3():
+#     less_two = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating < 4.25).filter(TripAdvisor.price == '$$$').all()[0][0]
+#     two_to_four = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.25).filter(TripAdvisor.rating < 4.75).filter(TripAdvisor.price == '$$$').all()[0][0]
+#     greater_four = one_to_three = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.75).filter(TripAdvisor.price == '$$$').all()[0][0]
+#     return [less_two, two_to_four, greater_four]
+
+def ta_price_rating_hist_4():
+    less_two = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating < 4.25).filter(TripAdvisor.price == '$$$$').all()[0][0]
+    two_to_four = db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.25).filter(TripAdvisor.rating < 4.75).filter(TripAdvisor.price == '$$$$').all()[0][0]
+    greater_four =  db.session.query(func.count(Restaurant.name)).join(TripAdvisor).filter(TripAdvisor.rating >= 4.75).filter(TripAdvisor.price == '$$$$').all()[0][0]
+    return [less_two, two_to_four, greater_four]
+
+def inf_rating(neighborhood):
+    rating_list = []
+    for i in db.session.query(Infatuation.rating).join(Restaurant).join(Neighborhood).filter(Neighborhood.name == neighborhood).all():
+        rating_list.append(i[0])
+    return rating_list
+
+def map_data():
+    map_list = []
+    for i in db.session.query(Restaurant.name, Restaurant.latitude, Restaurant.longitude, TripAdvisor.price, TripAdvisor.rating).join(TripAdvisor).filter(Restaurant.latitude != ' ').all():
+        map_list.append(i)
+    return map_list
+
+def ta_rating(neighborhood):
+    rating_list = []
+    for i in db.session.query(TripAdvisor.rating).join(Restaurant).join(Neighborhood).filter(Neighborhood.name == neighborhood).all():
+        rating_list.append(i[0])
+    return rating_list
+def rating_comparison(): #restaurant name, mich stars, trip advisor rating, infatuation rating
+    q = db.session.query(Restaurant.name, Infatuation.rating, TripAdvisor.rating).join(TripAdvisor).join(Infatuation).group_by(Restaurant.name).order_by(Restaurant.name).all()
+    return {'text': [i[0] for i in q], 'x':[i[1]/2 for i in q], 'y':[i[2] for i in q]}
+
+def rating_price_inf(): #restaurant name, mich stars, trip advisor rating, infatuation rating
+    q = db.session.query(Restaurant.name, Infatuation.price, Infatuation.rating).join(Infatuation).group_by(Restaurant.name).order_by(Restaurant.name).all()
+    return {'text': [i[0] for i in q], 'x':[i[1] for i in q], 'y':[i[2]/2 for i in q]}
+
+def rating_price_ta(): #restaurant name, mich stars, trip advisor rating, infatuation rating
+    q = db.session.query(Restaurant.name, TripAdvisor.rating, TripAdvisor.price).join(TripAdvisor).group_by(Restaurant.name).order_by(Restaurant.name).all()
+    return {'text': [i[0] for i in q], 'x':[i[2] for i in q], 'y':[i[1] for i in q]}
+
+def rating_hist_1():
+    return db.session.query(func.count(Restaurant.name)).join(Infatuation).filter(Infatuation.price == '$').all()[0]
